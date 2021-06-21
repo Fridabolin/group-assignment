b6a77523d8b965e8794e1ce7ec1ace3e44bb0fce
Fridabolin <80760258+Fridabolin@users.noreply.github.com>
2021-06-21 08:59:27 +0200

    done

--- main.js
+++ main.js
@@ -3,20 +3,14 @@ const artist = document.getElementById('artist');
 const songTitle = document.getElementById('song');
 //-----------//
 const button = document.getElementById('button');
-const outputLyrics = document.getElementById('lyricsArea');
-const showErrorTitle = document.getElementById("error-title");
+const outputText = document.getElementById('lyricsArea');
 
 
 function errorMessagesTitle(){ 
-  let messages = "" // = error messages //
   
   if (songTitle.value === '' || songTitle === null) {
-    messages = 'You must type in a song title.'
-  } 
-
-  if (messages.length > 0) {
-    showErrorTitle.innerText = messages  
-    return false
+   outputText.innerText = messages = 'You must type in a song title.'
+   return false
   } 
   return true
 }
@@ -27,10 +21,10 @@ artist.addEventListener("keyup", function(event){
   const value = event.target.value 
 
   if (value.length >=1 && value != " "){
-    outputLyrics.innerText= ""
+    outputText.innerText= ""
     button.removeAttribute("disabled")
   } else {
-    outputLyrics.innerText= "Button won't work whitout an Artist "
+    outputText.innerText= "Button won't work without an Artist "
     button.setAttribute("disabled",1) 
   }
 })
@@ -44,16 +38,14 @@ artist.addEventListener("keyup", function(event){
    if( errorMessage === true){
     const inputArtist = artist.value;
     const inputTitle = songTitle.value;
-    messages = " "
-    showErrorTitle.innerText = messages  
-
+    
     fetch(`http://ianertson.com:3500/${inputArtist}/${inputTitle}`).then(function(response){
       response.json().then(data => {
-
+        
         if(data.length === 0){
-          outputLyrics.innerText= "uh oh! we couldn't find your lyrics"
+          outputText.innerText= "uh oh! we couldn't find your lyrics"
         } else {
-          outputLyrics.innerText = data[0].lyrics
+          outputText.innerText = data[0].lyrics
         }
       })
     })
